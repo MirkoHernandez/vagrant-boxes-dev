88c37578a4b876e2dd198cf8a9f811339a1a0e6d
Mirko Hernandez <mirkoh@fastmail.com>
2019-01-21 12:27:41 -0500

    Include basic postgresql box.

---
+++ postgres/provisioning/provision-linux.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+POSTGRES_VERSION=$1
+
+touch ~vagrant/.bash_aliases
+
+sudo apt-get update
+sudo apt-get install  -y postgresql-$POSTGRES_VERSION postgresql-contrib-$POSTGRES_VERSION
