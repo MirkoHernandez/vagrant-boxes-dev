4674e9c219b036e402def1d79bc7c0b68444c502
Mirko Hernandez <mirkoh@fastmail.com>
2019-01-26 16:24:21 -0500

    Include git and tmux in  provision-linux.sh.

--- postgres/provisioning/provision-linux.sh
+++ postgres/provisioning/provision-linux.sh
@@ -5,4 +5,7 @@ POSTGRES_VERSION=$1
 touch ~vagrant/.bash_aliases
 
 sudo apt-get update
+sudo apt-get install  -y git tmux
+
+# Install postgres
 sudo apt-get install  -y postgresql-$POSTGRES_VERSION postgresql-contrib-$POSTGRES_VERSION
