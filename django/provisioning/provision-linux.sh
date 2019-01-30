3084594f505897e5202283ea07d462e2bf5f55ce
Mirko Hernandez <mirkoh@fastmail.com>
2019-01-29 20:09:18 -0500

    Include tmux in linux provisioning.

--- django/provisioning/provision-linux.sh
+++ django/provisioning/provision-linux.sh
@@ -3,4 +3,13 @@
 sudo apt-get update
 sudo apt-get install -y git tmux
 
+
+
+# tmux config
+touch ~vagrant/.tmux.conf
+printf "set -g base-index 1\n" >> ~vagrant/.tmux.conf
+
+# Aliases
 touch ~vagrant/.bash_aliases
+
+
--- postgres/provisioning/provision-linux.sh
+++ postgres/provisioning/provision-linux.sh
@@ -2,10 +2,17 @@
 
 POSTGRES_VERSION=$1
 
-touch ~vagrant/.bash_aliases
 
 sudo apt-get update
 sudo apt-get install  -y git tmux
 
 # Install postgres
 sudo apt-get install  -y postgresql-$POSTGRES_VERSION postgresql-contrib-$POSTGRES_VERSION
+
+# tmux config
+touch ~vagrant/.tmux.conf
+printf "set -g base-index 1\n" >> ~vagrant/.tmux.conf
+
+
+# Aliases
+touch ~vagrant/.bash_aliases
