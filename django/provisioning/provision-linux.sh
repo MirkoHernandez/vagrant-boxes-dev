9a877aa0f8005c2c7b24609e373d0aeadcad9b1a
Mirko Hernandez <mirkoh@fastmail.com>
2019-04-26 14:48:33 -0400

    Include some comments in vagrant config and provisioning.

--- django/provisioning/provision-linux.sh
+++ django/provisioning/provision-linux.sh
@@ -7,6 +7,7 @@ sudo apt-get install -y git tmux
 
 # tmux config
 touch ~vagrant/.tmux.conf
+# start naming panes from 1 instead of 0. 
 printf "set -g base-index 1\n" >> ~vagrant/.tmux.conf
 
 # Aliases
